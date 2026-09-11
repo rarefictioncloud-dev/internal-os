ad0b25d655be5b8e91d66238a4e8567f7997041f
Advaitkasturi <advaitkasturi20@gmail.com>
2026-09-11 15:49:14 +0530

    changes made

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  base: '/internal-os/',
+  plugins: [react(), tailwindcss()],
 })

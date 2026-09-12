b53ec78b253ce8e74bf0b18a96ffee9f04f083c7
Advaitkasturi <advaitkasturi20@gmail.com>
2026-09-12 22:29:09 +0530

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  base: '/internal-os/',
-  assetsInclude: ['**/*.glb'],
-  plugins: [react(), tailwindcss()],
-})
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/internal-os/' : '/',
+
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+}))
